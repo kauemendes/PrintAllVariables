1ee14d5c83177b5550e6018043369d56bdf7824e
x351971 <x351971@bcpcorp.net>
2020-02-05 13:14:02 +0000

    Improvements on PrintAllVariables package

--- printAllVariables/printAllVariables.js
+++ printAllVariables/printAllVariables.js
@@ -10,11 +10,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const tl = require("azure-pipelines-task-lib/task");
 const util_1 = require("util");
+function arrayContains(needle, arrhaystack) {
+    return (arrhaystack.indexOf(needle) > -1);
+}
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             //do your actions
             const allVariables = tl.getVariables();
+            let variablesToFilter = tl.getDelimitedInput('filtered_variables', '\n', false);
+            let lchangeVariableValue = tl.getBoolInput('change_variable_value');
+            let lchangeVariableName = tl.getBoolInput('change_variable_name');
             let typeVariableTransformation = tl.getInput("type_input_transform");
             let separatorIntoVariable = tl.getInput("separator_into_variable");
             if (util_1.isNullOrUndefined(separatorIntoVariable)) {
@@ -29,37 +35,39 @@ function run() {
                 }
                 return 0;
             });
+            if (variablesToFilter.length === 0) {
+                console.log("INFO: None variables will be manipulate, only printing...");
+            }
+            variablesToFilter = variablesToFilter.map(function (x) { return x.toUpperCase(); });
             console.log("INFO: All the variables in this format: [variable name] => value");
             sortedArray.forEach(element => {
                 let newValue = element.value;
                 let newName = element.name;
-                if (!util_1.isNullOrUndefined(separatorIntoVariable)) {
-                    // Changes all occurrancies
-                    newName = newName.replace(/\_/g, separatorIntoVariable);
-                    newName = newName.replace(/\-/g, separatorIntoVariable);
-                    newName = newName.replace(/\./g, separatorIntoVariable);
-                    newName = newName.replace(/\>/g, separatorIntoVariable);
-                    newName = newName.replace(/\</g, separatorIntoVariable);
-                    newName = newName.replace(/\:/g, separatorIntoVariable);
+                if (arrayContains(element.name.toUpperCase(), variablesToFilter)) {
+                    if (!util_1.isNullOrUndefined(separatorIntoVariable) && lchangeVariableName) {
+                        // Changes all occurrancies
+                        newName = newName.replace(/\_/g, separatorIntoVariable);
+                        newName = newName.replace(/\-/g, separatorIntoVariable);
+                        newName = newName.replace(/\./g, separatorIntoVariable);
+                        newName = newName.replace(/\>/g, separatorIntoVariable);
+                        newName = newName.replace(/\</g, separatorIntoVariable);
+                        newName = newName.replace(/\:/g, separatorIntoVariable);
+                    }
+                    switch (typeVariableTransformation) {
+                        case 'lower':
+                            newValue = lchangeVariableValue ? newValue.toLowerCase() : newValue;
+                            newName = lchangeVariableName ? newName.toLowerCase() : newName;
+                            break;
+                        case 'upper':
+                            newValue = lchangeVariableValue ? newValue.toUpperCase() : newValue;
+                            newName = lchangeVariableName ? newName.toUpperCase() : newName;
+                            break;
+                        default:
+                            break;
+                    }
+                    tl.setTaskVariable(newName, newValue, element.secret);
+                    tl.setVariable(newName, newValue, element.secret);
                 }
-                switch (typeVariableTransformation) {
-                    case 'lower':
-                        if (!element.secret)
-                            newValue = newValue.toLowerCase();
-                        newName = newName.toLowerCase();
-                        break;
-                    case 'upper':
-                        if (!element.secret)
-                            newValue = newValue.toUpperCase();
-                        newName = newName.toUpperCase();
-                        break;
-                    default:
-                        break;
-                }
-                tl.setTaskVariable(newName, newValue, element.secret);
-                tl.setVariable(newName, newValue, element.secret);
-                if (element.secret)
-                    console.log(">>>>>> This is a SECRET, value not changed <<<<<<");
                 console.log(newName + " => " + newValue);
             });
             tl.setResult(tl.TaskResult.Succeeded, 'DONE');
--- printAllVariables/printAllVariables.ts
+++ printAllVariables/printAllVariables.ts
@@ -1,10 +1,18 @@
 import tl = require('azure-pipelines-task-lib/task');
 import { isNullOrUndefined } from 'util';
 
+function arrayContains(needle, arrhaystack)
+{
+    return (arrhaystack.indexOf(needle) > -1);
+}
+
 async function run() {
     try {
         //do your actions
         const allVariables = tl.getVariables();
+        let variablesToFilter: string[] = tl.getDelimitedInput('filtered_variables', '\n', false);
+        let lchangeVariableValue: Boolean = tl.getBoolInput('change_variable_value')
+        let lchangeVariableName: Boolean = tl.getBoolInput('change_variable_name')
         let typeVariableTransformation = tl.getInput("type_input_transform");
         let separatorIntoVariable = tl.getInput("separator_into_variable");
 
@@ -22,39 +30,46 @@ async function run() {
             }
             return 0;
         });
+
+        if(variablesToFilter.length === 0) {
+            console.log("INFO: None variables will be manipulate, only printing...");
+        }
+
+        variablesToFilter = variablesToFilter.map(function(x){ return x.toUpperCase() })
+
         console.log("INFO: All the variables in this format: [variable name] => value")
         sortedArray.forEach(element => {
             let newValue = element.value;
             let newName = element.name;
+            if (arrayContains(element.name.toUpperCase(), variablesToFilter)) {
+                if (!isNullOrUndefined(separatorIntoVariable) && lchangeVariableName) {
+                    // Changes all occurrancies
+                    newName = newName.replace(/\_/g, separatorIntoVariable);
+                    newName = newName.replace(/\-/g, separatorIntoVariable);
+                    newName = newName.replace(/\./g, separatorIntoVariable);
+                    newName = newName.replace(/\>/g, separatorIntoVariable);
+                    newName = newName.replace(/\</g, separatorIntoVariable);
+                    newName = newName.replace(/\:/g, separatorIntoVariable);
+                }
+                
+                switch (typeVariableTransformation) {
+                    case 'lower':
+                        newValue = lchangeVariableValue ? newValue.toLowerCase() : newValue
+                        newName = lchangeVariableName ? newName.toLowerCase() : newName
+                        break;
 
-            if (!isNullOrUndefined(separatorIntoVariable)) {
-                // Changes all occurrancies
-                newName = newName.replace(/\_/g, separatorIntoVariable);
-                newName = newName.replace(/\-/g, separatorIntoVariable);
-                newName = newName.replace(/\./g, separatorIntoVariable);
-                newName = newName.replace(/\>/g, separatorIntoVariable);
-                newName = newName.replace(/\</g, separatorIntoVariable);
-                newName = newName.replace(/\:/g, separatorIntoVariable);
+                    case 'upper':
+                        newValue = lchangeVariableValue ? newValue.toUpperCase() : newValue 
+                        newName = lchangeVariableName ? newName.toUpperCase() : newName
+                        break;
+                
+                    default:
+                        break;
+                }
+
+                tl.setTaskVariable(newName, newValue, element.secret);
+                tl.setVariable(newName, newValue, element.secret);
             }
-            
-            switch (typeVariableTransformation) {
-                case 'lower':
-                    if(!element.secret) newValue = newValue.toLowerCase()
-                    newName = newName.toLowerCase()
-                    break;
-
-                case 'upper':
-                    if(!element.secret) newValue = newValue.toUpperCase()
-                    newName = newName.toUpperCase()
-                    break;
-            
-                default:
-                    break;
-            }
-
-            tl.setTaskVariable(newName, newValue, element.secret);
-            tl.setVariable(newName, newValue, element.secret);
-            if(element.secret) console.log(">>>>>> This is a SECRET, value not changed <<<<<<");
             console.log(newName + " => " + newValue);
         });
         tl.setResult(tl.TaskResult.Succeeded, 'DONE');
